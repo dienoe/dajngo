478db5fb22c5140232aebc0f2fc0c10dcd9e1225
x <=410947380@qq.com>
2019-07-04 14:32:44 +0800

    celery异步发送短信定义实现

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+# url(r'^users/$', views.UserView.as_view()),
+from rest_framework.generics import CreateAPIView
+
+
+class UserView(CreateAPIView):
+    """
+    用户注册
+    传入参数：
+        username, password, password2, sms_code, mobile, allow
+    """
+    pass
